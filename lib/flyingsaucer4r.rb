fce7ac05963f5f9004f51eb6db5328cbf7d5de16
Angel N. Sciortino <contact@angeliccomputing.com>
2009-10-14 14:41:11 -0500

    refactored flyingsaucer4r.rb

--- lib/flyingsaucer4r.rb
+++ lib/flyingsaucer4r.rb
@@ -22,30 +22,12 @@ module FlyingSaucer4R
   end
 
   def self.create_pdf(xhtml, base_path, logger = nil)
-    begin
-      dom = create_java_dom(xhtml) 
-    rescue NativeException => e
-      java_e = e.cause
-      if java_e.is_a?(org.xml.sax.SAXParseException)
-        context = provide_context(xhtml, java_e.line_number) 
-        logger.info("Unable to parse XHTML at line #{java_e.line_number}, column #{java_e.column_number}: #{java_e.message}\n#{context}") if logger
-      end
-      raise e
-    end
     estimated_pdf_length = xhtml.length
     output = java.io.ByteArrayOutputStream.new(estimated_pdf_length)
 
     begin
-      renderer = org.xhtmlrenderer.pdf.ITextRenderer.new
-      agent = UserAgent.new(renderer.output_device)
-      agent.shared_context = renderer.shared_context
-      renderer.shared_context.user_agent_callback = agent
-      renderer.set_document(dom, path_to_url(base_path))
-      renderer.layout
-
-      renderer.create_pdf(output)
-
-      String.from_java_bytes(output.to_byte_array)
+      dom = create_java_dom(xhtml, logger)
+      render_pdf(dom, base_path, output)
     ensure
       output.close
     end
@@ -76,9 +58,30 @@ module FlyingSaucer4R
     result.join('')
   end
 
-  def self.create_java_dom(s)
-    builder = javax.xml.parsers.DocumentBuilderFactory.new_instance.new_document_builder
-    builder.parse(java.io.ByteArrayInputStream.new(s.to_java_bytes))
+  def self.create_java_dom(s, logger)
+    begin
+      builder = javax.xml.parsers.DocumentBuilderFactory.new_instance.new_document_builder
+      builder.parse(java.io.ByteArrayInputStream.new(s.to_java_bytes))
+    rescue NativeException => e
+      java_e = e.cause
+      if java_e.is_a?(org.xml.sax.SAXParseException)
+        context = provide_context(s, java_e.line_number) 
+        logger.info("Unable to parse XHTML at line #{java_e.line_number}, column #{java_e.column_number}: #{java_e.message}\n#{context}") if logger
+      end
+      raise e
+    end
+  end
+
+  def self.render_pdf(dom, base_path, output)
+    renderer = org.xhtmlrenderer.pdf.ITextRenderer.new
+    agent = UserAgent.new(renderer.output_device)
+    agent.shared_context = renderer.shared_context
+    renderer.shared_context.user_agent_callback = agent
+    renderer.set_document(dom, path_to_url(base_path))
+    renderer.layout
+
+    renderer.create_pdf(output)
+    String.from_java_bytes(output.to_byte_array)
   end
 
   def self.path_to_url(path)
